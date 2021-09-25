51b3ac6de35af28f20496e344739d434608e80f4
Agustina Olmos <olmos.agustina@gmail.com>
2021-09-25 11:00:33 -0300

    solucion servidor express

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const express = require('express')
 const path = require('path')
 
+// Crear el servidor de express
 const app = express()
 
-const staticFiles = express.static('public')
-app.use(staticFiles)
+// Directorio Publico
+app.use(express.static('public'))
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000')
-})
-
+// Rutas
 app.get('/', (req, res) =>{
-    const fileToSend = path.join(__dirname,'/views/home.html')
-    res.sendFile(fileToSend)
-    //res.sendFile(path.join(__dirname,'/views/home.html'))
+    res.sendFile(path.join(__dirname,'/views/home.html'))
 })
+app.get('/register', (req, res) =>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
 
-
-/* app.get('/crear-cuenta', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/views/register.html'))
 })
-app.post('/crear-cuenta', (req, res) =>{
-    res.redirect('/')
-}) */
+app.post('/register', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
-
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Servidor Express corriendo en: ', 'http://localhost:3000')
+})
